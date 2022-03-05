a72bc873c951c02d225e702e7f8e687fa81bb29d
developer-andreluiz <developer.andreluiz@gmail.com>
2022-03-05 05:47:35 -0300

    Inicio projeto

---
+++ Trabalho_App_Mercado_Tittio/Pages/LoadPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Trabalho_App_Mercado_Tittio.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoadPage : ContentPage
+    {
+        public LoadPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
